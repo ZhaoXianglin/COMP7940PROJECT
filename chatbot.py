4cc447754ba80e9c37dfb371bd2bd486a48ffd89
Zhaoxianglin <sisivip@vip.qq.com>
2021-04-21 14:35:21 +0800

    add address and weather

--- chatbot.py
+++ chatbot.py
@@ -21,7 +21,7 @@ def run():
     dispatcher = updater.dispatcher
     echo_handler = MessageHandler(Filters.photo, image_handler)
     location_handler = MessageHandler(Filters.location, returnlocation)
-    dispatcher.add_handler(CommandHandler('hello',hello))
+    dispatcher.add_handler(CommandHandler('hello', hello))
     dispatcher.add_handler(echo_handler)
     dispatcher.add_handler(location_handler)
     dispatcher.add_handler(CommandHandler('location', location))
@@ -38,16 +38,16 @@ def hello(bot, update):
     update.bot.send_message(chat_id=bot.effective_chat.id, text=reply_message)
 
 
-def returnlocation(bot,update):
+def returnlocation(bot, update):
     replay_message = "打卡成功！"
     update.bot.send_message(chat_id=bot.effective_chat.id, text=replay_message)
-
-
     print("打卡成功！")
+
+
 def location(bot, update):
     reply_markup = ReplyKeyboardMarkup(
         [[KeyboardButton('共享位置', request_location=True)]],
-        resize_keyboard= True,
+        resize_keyboard=True,
         one_time_keyboard=True,
     )
     message = "请共享你的打卡位置"
@@ -56,27 +56,19 @@ def location(bot, update):
 
 def image_handler(bot, update):
     file = bot.message.photo[-1].file_id
-    #print(type(FileExistsError))
-
-    #ile_info=requests.get('https://api.telegram.org/bot{0}/getFile?file_id={1}'.format(API_TOKEN, file_info))
-    #file_info=file_info.file_path
-    #file_dict = json.load(file_info)
-    #print(file_info.file_path)
-    #file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(API_TOKEN, file_info.file_path))
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     file = updater.bot.get_file(file)
     file.download("1.jpg")
-    with open("1.jpg",'rb') as photo:
+    with open("1.jpg", 'rb') as photo:
         output=calorie(photo.read())
-    #calorie("1.jpg")
     print(output)
-
     if str(output['result'][0]['has_calorie'])=='False':
 
         update.bot.send_message(chat_id=bot.effective_chat.id, text="Can not recognize any food in the image")
     else:
-        replay_message="The calorie of this food is "+output['result'][0]['calorie'] +" kj"
+        replay_message="The calorie of this food is "+output['result'][0]['calorie'] + " kj"
         update.bot.send_message(chat_id=bot.effective_chat.id, text=replay_message)
+
     #print(output['result'][0]['calorie'])
 
 
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import configparser
 import logging
-from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import Updater, Dispatcher, MessageHandler, Filters, CommandHandler, CallbackContext
 from kkbox_developer_sdk.auth_flow import KKBOXOAuth
 from kkbox_developer_sdk.api import KKBOXAPI
+import random
+import urllib
+import requests
 
 # 载入配置文件
 config = configparser.ConfigParser()
@@ -15,6 +18,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
+# 主程序
 def run():
 
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
@@ -24,7 +28,9 @@ def run():
     # 处理所有文字请求
     text_handler = MessageHandler(Filters.text & (~Filters.command), nlp)
     dispatcher.add_handler(text_handler)
-
+    # 处理地理位置请求
+    location_handler = MessageHandler(Filters.location, returnlocation)
+    dispatcher.add_handler(location_handler)
     # 处理图片卡路里计算
     cal_handler = MessageHandler(Filters.photo, image_handler)
     dispatcher.add_handler(cal_handler)
@@ -34,6 +40,22 @@ def run():
     updater.idle()
 
 
+# 打卡成功
+def returnlocation(bot, update):
+    key = config['MAP']['KEY']
+    url = 'https://restapi.amap.com/v3/geocode/regeo'
+    params = {
+        "location": str(bot.message['location']['longitude'])+','+str(bot.message['location']['latitude']),
+        "output": 'json',
+        "key": key,
+    }
+    r = requests.get(url=url, params=params)
+    add = r.json()['formatted_address']
+    replay_message = "打卡成功！"
+    update.bot.send_message(chat_id=bot.effective_chat.id, text=replay_message)
+
+
+# 欢迎信息
 def welcome(update, context):
     welcome_message = '''欢迎使用运动助手机器人,您可以这样和我说
                         - 帮我计算我的BMI 
@@ -43,15 +65,17 @@ def welcome(update, context):
                         - 户外运动打卡 
                         也可以试着和我闲聊，比如今天天气如何？
     '''
-    reply_keyboard_markup = ReplyKeyboardMarkup([['香港今天天气如何'],['给我讲个笑話'],['你好吗'],['给我来点运动时听的音乐'],['瑜伽要怎么做']])
+    reply_keyboard_markup = ReplyKeyboardMarkup([['香港今天天气如何'], ['给我讲个笑话吧'], ['你好吗'], ['给我来点运动时听的音乐'], ['瑜伽要怎么做']])
+    user = update.message.from_user
+    print('You talk with user {} and his user ID: {} '.format(user['username'], user['id']))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_message, reply_markup=reply_keyboard_markup)
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_message, reply_markup = reply_keyboard_markup)
 
-
-# 所有的文字信息转发dialogflow
+# 所有的文字信息转发dialogflow处理
 def nlp(update, context):
     msg_text = update.message.text
     intent, res_text = detect_intent_texts("comp7930-final-ipxa", '123456789', msg_text, 'zh-CN')
+    print(intent, '---')
     if intent == 'music':
         print("在这")
         auth = KKBOXOAuth(config['KKBOX']['ID'], config['KKBOX']['SECRET'])
@@ -59,12 +83,29 @@ def nlp(update, context):
         kkboxapi = KKBOXAPI(token)
         search_results = kkboxapi.search_fetcher.search('workout', types=['playlist'], terr='HK')
         playlists = search_results['playlists']['data']
-        first = playlists[0]
+        rdmnumber = random.randint(0, len(playlists)-1)
+        first = playlists[rdmnumber]
         print(first)
         context.bot.send_message(chat_id=update.effective_chat.id, text=first['title'])
         context.bot.send_photo(chat_id=update.effective_chat.id,photo=first['images'][0]["url"])
         context.bot.send_message(chat_id=update.effective_chat.id, text=first['url'])
+    elif intent == 'Default Fallback Intent':
+        # 找不到意图的，就转发给机器人处理
+        url = 'http://api.qingyunke.com/api.php?key=free&appid=0&msg={}'.format(urllib.parse.quote(msg_text))
+        res = requests.get(url)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=res.json()["content"])
+
+    elif intent == 'card':
+        # 调用打卡意图
+        reply_markup = ReplyKeyboardMarkup(
+            [[KeyboardButton('共享位置', request_location=True)]],
+            resize_keyboard=True,
+            one_time_keyboard=True,
+        )
+        message = "请共享你的打卡位置"
+        context.bot.send_message(chat_id=update.message.chat_id, text=message, reply_markup=reply_markup)
     else:
+        # print("hellwo")
         context.bot.send_message(chat_id=update.effective_chat.id, text=res_text)
 
 
@@ -73,7 +114,7 @@ def image_handler(bot, update):
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     file = updater.bot.get_file(file)
     file.download()
-    print(file, type(update),type(bot))
+    print(file, type(update), type(bot))
     print(bot.message.photo[-1])
 
 
